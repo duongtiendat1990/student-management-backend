89078a0e9ea81aca3f72b52ac537ba0beaf6dd8e
duongtiendat1990 <duongtiendat1990@gmail.com>
2019-09-05 17:20:01 +0700

    design user resource

--- src/main/java/com/sopen/studentmanagement/controllers/UserController.java
+++ src/main/java/com/sopen/studentmanagement/controllers/UserController.java
@@ -5,20 +5,35 @@ import com.sopen.studentmanagement.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/api/user")
 public class UserController {
   @Autowired
   UserService userService;
 
-  @GetMapping("/users/{id}")
+  @GetMapping("/{id}")
+  @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_STUDENT')")
   public ResponseEntity<User> getUser(@PathVariable Long id) {
     User user = userService.findById(id);
     return new ResponseEntity < User > (user,HttpStatus.OK);
   }
+
+  @GetMapping
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  public ResponseEntity<List<User>> getAllStudent(){
+    return null;
+  }
+
+  @PostMapping
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  public ResponseEntity<?> createStudent(@RequestBody User user){
+    return null;
+  }
+
 }
